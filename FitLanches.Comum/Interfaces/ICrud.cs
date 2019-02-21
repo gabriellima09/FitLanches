37b7af25cc2d8e64e2dac9f03b8d444dfc927b5b
Gabriel Lima <gabriellimagomes.14@hotmail.com>
2019-02-20 21:46:11 -0300

    Adicionadas interfaces de uso do sistema

---
+++ FitLanches.Comum/Interfaces/ICrud.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitLanches.Comum.Interfaces
+{
+    public interface ICrud<T> where T : class
+    {
+        void Inserir(T entidade);
+        void Alterar(T entidade);
+        void Excluir(int? id);
+        IList<T> SelecionarTodos();
+        T SelecionarPorId(int id);
+    }
+}
---
+++ FitLanches.Dominio/Interfaces/IPedido.cs
@@ -0,0 +1,16 @@
+﻿using FitLanches.Dominio.Enums;
+using FitLanches.Dominio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitLanches.Dominio.Interfaces
+{
+    public interface IPedido
+    {
+        void GerarNovoPedido(IList<Item> itens);
+        void TrocarStatusPedido(StatusPedido status);
+    }
+}
